afaa36c83af9f287d44800e2c4eaca5750def06b
Sucheta <suchi0208@gmail.com>
2020-06-24 17:51:02 +0530

    third commit

---
+++ src/main/java/GitHubPractice/gitpract/class3.java
@@ -0,0 +1,10 @@
+package GitHubPractice.gitpract;
+
+public class class3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("class 3 hello");
+	}
+
+}
